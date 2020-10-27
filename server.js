5b20dec74166a491c3c098000fc1ee86025467cb
antekirta <murashkin.kirill.konst@gmail.com>
2020-10-27 21:31:41 +0200

    Init

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const port = process.argv[2];
+
+const server = http.createServer((req, res) => {
+  res.end('Hello, world!');
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
