78f96556d72621a5892fc9830ef0ab54635412a4
antekirta <murashkin.kirill.konst@gmail.com>
2020-10-28 20:56:26 +0200

    Docker compose

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN npm install
 
 COPY . /usr/src/app
 
-EXPOSE 8080
+EXPOSE 9000
 
 ENV PORT 9000
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 9000;
 
 console.log(process.env);
 
