e080d0d889c583586c9414310f405138ae1ceca7
antekirta <murashkin.kirill.konst@gmail.com>
2020-10-31 19:04:02 +0200

    Add -L flag to nodemon command

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,9 @@
 
 sudo apt-get update
 
-sudo apt-get install build-essential libssl-dev
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
-bash install_nvm.sh
-# shellcheck disable=SC1090
-source ~/.profile
-nvm install 6.0.0
-nvm install 14.15.0
+sudo apt install build-essential checkinstall libssl-dev
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.1/install.sh | bash
+nvm install 14
 
 sudo npm i
 npm i typescript -g
