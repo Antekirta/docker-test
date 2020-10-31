844b10f82099aec168c24274257498307a0ed9b9
antekirta <murashkin.kirill.konst@gmail.com>
2020-10-31 20:21:03 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,12 @@ sudo apt-get update
 
 sudo apt install build-essential checkinstall libssl-dev
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.1/install.sh | bash
-nvm install 14
+export NVM_DIR="$HOME/.nvm"
+# shellcheck disable=SC1090
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# shellcheck disable=SC1090
+source ~/.nvm/nvm.sh install 14
 
-sudo npm i
-npm i typescript -g
-npm i ts-node -g
+cd /vagrant || exit
+
+npm i --no-bin-links
