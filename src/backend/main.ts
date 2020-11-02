b58112c5056f754c8911f959cb364d8692ff7b6d
antekirta <murashkin.kirill.konst@gmail.com>
2020-11-02 17:45:22 +0200

    Create front-end

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -1,11 +1,9 @@
 import http, { IncomingMessage, ServerResponse } from 'http';
 
 const server = http.createServer((req : IncomingMessage, res: ServerResponse) => {
-  res.end('Hello, Emma!');
+  res.end('Hello, maaan!');
 });
 
 server.listen(9000, () => {
   console.log('Server is truly listening on port 9000');
 });
-
-
